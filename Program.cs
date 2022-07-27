c4209d83a557f4212d094406aa22e034a5a4a866
mandara11 <mandaram2345@gmail.com>
2022-07-27 08:57:38 +0530

    Added:Program

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace FlipTheCoin
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the number of Flips");
+
+            int flips = Convert.ToInt32(Console.ReadLine());
+
+            Flip.CalculatePercentage();
+
+            Flip.Display();   
+        }    
+    }
+}
